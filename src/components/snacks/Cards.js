96beba868231b516e951268794a598dde9002767
Nate <ndsunc@gmail.com>
2018-09-06 14:14:34 -0700

    Updated inline HTML style from key/value pairs whose value is a string to boolean values

--- src/components/snacks/Cards.js
+++ src/components/snacks/Cards.js
@@ -3,15 +3,17 @@ import {CardGroup} from 'reactstrap';
 import Card from './Card'
 
 const Cards = ({props}) => {
-  return (<CardGroup>
-    <div className="d-flex align-content-around flex-wrap">
-      {
-        props.map(card => {
-          return (<Card key={card.id} card={card}/>)
-        })
-      }
-    </div>
-  </CardGroup>)
+  return (
+    <CardGroup>
+      <div className="d-flex align-content-around flex-wrap">
+        {
+          props.map(card => {
+            return (<Card key={card.id} card={card}/>)
+          })
+        }
+      </div>
+    </CardGroup>
+  )
 }
 
 export default Cards
--- src/components/snacks/SnackForm.js
+++ src/components/snacks/SnackForm.js
@@ -12,6 +12,7 @@ import {
 } from 'reactstrap';
 
 const SnackForm = ({handleEditSnack, handleAddSnack, singleSnack}) => {
+  // console.log(handleEditSnack, handleAddSnack, singleSnack);
 
   const snack = {
     id: singleSnack
@@ -34,56 +35,58 @@ const SnackForm = ({handleEditSnack, handleAddSnack, singleSnack}) => {
       : ""
   }
 
-  return (<Form className="form-horizontal well" onSubmit={event => {
-      event.preventDefault();
-      snack.id
-        ? handleEditSnack(snack.id, event.target.title.value, event.target.description.value, event.target.price.value, event.target.img.value, (event.target.is_perishable.value || false))
-        : handleAddSnack(event.target.title.value, event.target.description.value, event.target.price.value, event.target.img.value, (event.target.is_perishable.value || false));
-    }}>
+  return (
+    <Form className="form-horizontal well" onSubmit={event => {
+        event.preventDefault();
+        snack.id
+          ? handleEditSnack(snack.id, event.target.title.value, event.target.description.value, event.target.price.value, event.target.img.value, (event.target.is_perishable.value || false))
+          : handleAddSnack(event.target.title.value, event.target.description.value, event.target.price.value, event.target.img.value, (event.target.is_perishable.value || false));
+      }}>
 
-    <FormGroup>
-      <Label for="title">Snack Name</Label>
-      <Input type="textarea" defaultValue={snack.name} name="title" id="title" required="required"/>
-    </FormGroup>
+      <FormGroup>
+        <Label for="title">Snack Name</Label>
+        <Input type="textarea" defaultValue={snack.name} name="title" id="title" required="required"/>
+      </FormGroup>
 
-    <FormGroup>
-      <Label for="description">Snack Description</Label>
-      <Input type="textarea" defaultValue={snack.description} name="description" id="description" required="required"/>
-    </FormGroup>
-    <FormGroup>
-      <Label for="url">Url</Label>
-      <Input type="url" name="img" defaultValue={snack.img} id="img" placeholder="url" required="required"/>
-    </FormGroup>
+      <FormGroup>
+        <Label for="description">Snack Description</Label>
+        <Input type="textarea" defaultValue={snack.description} name="description" id="description" required="required"/>
+      </FormGroup>
+      <FormGroup>
+        <Label for="url">Url</Label>
+        <Input type="url" name="img" defaultValue={snack.img} id="img" placeholder="url" required="required"/>
+      </FormGroup>
 
-    <FormGroup>
-      <Label for="price">Snack Price?</Label>
-      <InputGroup>
-        <InputGroupAddon addonType="prepend">$</InputGroupAddon>
-        <Input name="price" id="name" defaultValue={snack.price} placeholder="Amount" type="number" required="required"/>
-        <InputGroupAddon addonType="append">.00</InputGroupAddon>
-      </InputGroup>
-    </FormGroup>
-    <FormGroup>
-      <Label for="is_perishable">Is your snack perishable?</Label>
-      <div>
-        <CustomInput value="true" type="radio" id="true" name="is_perishable" label="Yes" inline="inline"/>
-        <CustomInput value="false" type="radio" id="false" name="is_perishable" label="No" inline="inline"/>
-      </div>
-    </FormGroup>
+      <FormGroup>
+        <Label for="price">Snack Price?</Label>
+        <InputGroup>
+          <InputGroupAddon addonType="prepend">$</InputGroupAddon>
+          <Input name="price" id="name" defaultValue={snack.price} placeholder="Amount" type="number" required="required"/>
+          <InputGroupAddon addonType="append">.00</InputGroupAddon>
+        </InputGroup>
+      </FormGroup>
+      <FormGroup>
+        <Label for="is_perishable">Is your snack perishable?</Label>
+        <div>
+          <CustomInput value="true" type="radio" id="true" name="is_perishable" label="Yes" inline/>
+          <CustomInput value="false" type="radio" id="false" name="is_perishable" label="No" inline/>
+        </div>
+      </FormGroup>
 
-    <FormGroup check="check">
-      <Col sm={{
-          size: 10,
-          offset: 0
-        }}>
-        <Button>{
-            snack.id
-              ? `Edit`
-              : `Add`
-          }</Button>
-      </Col>
-    </FormGroup>
-  </Form>);
+      <FormGroup check>
+        <Col sm={{
+            size: 10,
+            offset: 0
+          }}>
+          <Button>{
+              snack.id
+                ? `Edit`
+                : `Add`
+            }</Button>
+        </Col>
+      </FormGroup>
+    </Form>
+  )
 }
 
 export default SnackForm

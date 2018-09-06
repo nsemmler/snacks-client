6351c5a8d1781a38f1a4aa965e46d5eb6d4c45a5
Nate <ndsunc@gmail.com>
2018-09-06 13:30:36 -0700

    Updated format of return; Updated Button block param to be a boolean rather than a key/value pair

--- src/components/snacks/Card.js
+++ src/components/snacks/Card.js
@@ -11,33 +11,27 @@ import {
 } from 'reactstrap';
 
 class SingleCard extends React.Component {
-
   render() {
-    const {
-      card: {
-        id,
-        name,
-        description,
-        price,
-        img
-      }
-    } = this.props
+    const { card: { id, name, description, price, img } } = this.props
     const link = `/snack/${id}`
-    return (<Card sm="6" className="col-lg-4 col-md-2 col-sm-2">
-      <CardBody>
-        <CardTitle>{name}</CardTitle>
-        <CardSubtitle>${price}</CardSubtitle>
-      </CardBody>
-      <CardImg id="card_img" width="100%" src={img} alt="Card image cap"/>
-      <CardBody>
-        <CardText>{description}</CardText>
-        <Link to={link}>
-          <Button size="lg" block="block">
-            More Info
-          </Button>
-        </Link>
-      </CardBody>
-    </Card>)
+
+    return (
+      <Card sm="6" className="col-lg-4 col-md-2 col-sm-2">
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+          <CardSubtitle>${price}</CardSubtitle>
+        </CardBody>
+        <CardImg id="card_img" width="100%" src={img} alt="Card image cap"/>
+        <CardBody>
+          <CardText>{description}</CardText>
+          <Link to={link}>
+            <Button size="lg" block>
+              More Info
+            </Button>
+          </Link>
+        </CardBody>
+      </Card>
+    )
   }
 }
 
